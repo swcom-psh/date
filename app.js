d0ad0e9ce0b4eff2ec7090eec6a085a8848a4ed1
swpsh <sfgussf@naver.com>
2026-06-15 09:02:38 +0900

    123

--- app.js
+++ app.js
@@ -585,8 +585,8 @@ function renderSummary() {
     return;
   }
 
-  // Sort by availability count (desc)
-  datesWithAvail.sort((a, b) => b.availMembers.length - a.availMembers.length);
+  // Sort by date (asc)
+  datesWithAvail.sort((a, b) => a.day - b.day);
 
   const monthNames = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
   const dayNames = ['일', '월', '화', '수', '목', '금', '토'];
